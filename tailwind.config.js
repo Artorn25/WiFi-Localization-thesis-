410310582ca52c6adfc414d49da0a8f345e9f3fa
Artorn25 <sskjj0547@gmail.com>
2025-04-22 23:34:25 +0700

    delete: file not necessary

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}", "./pages/dashboard/.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
