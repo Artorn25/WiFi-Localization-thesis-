f8b97f1db3da791a2a0294bee8d092ef78388941
Artorn25 <sskjj0547@gmail.com>
2025-05-13 23:54:51 +0700

    style: add loading & add responsive popup

--- my-app/src/app/_components/ConditionalLayout.js
+++ my-app/src/app/_components/ConditionalLayout.js
@@ -4,17 +4,37 @@ import { usePathname } from "next/navigation";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 import ReportButton from "@components/ReportButton";
+import Loading from "@components/Loading";
+import { useEffect, useState } from "react";
 
 export default function ConditionalLayout({ children }) {
   const pathname = usePathname();
   const isHomePage = pathname === "/";
+  const [isInitialLoading, setIsInitialLoading] = useState(true);
+
+  useEffect(() => {
+    const initialize = async () => {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 2500));
+      } finally {
+        setIsInitialLoading(false);
+      }
+    };
+    initialize();
+  }, []);
 
   return (
     <>
-      {!isHomePage && <Header />}
-      {children}
-      <ReportButton />
-      <Footer />
+     {isInitialLoading ? (
+          <Loading />
+        ) : (
+          <>
+            {!isHomePage && <Header />}
+            {children}
+            <ReportButton />
+            <Footer />
+          </>
+        )}
     </>
   );
 }
